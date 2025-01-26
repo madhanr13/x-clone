d38b8dceb200b4b20588b2329ccde51b71fda56f
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-26 20:00:32 +0530

    add routing, loading spinners, and skeleton components for posts and profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.jsx";
+import SignupPage from "./pages/auth/signup/SignupPage.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
 
 const App = () => {
-  return <div className="bg-blue-500">App</div>;
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel/>
+    </div>
+  );
 };
 
 export default App;
