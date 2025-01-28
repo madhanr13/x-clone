9e3d730d9e9e3b45866d7c43aefe61af8f7c187e
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-28 19:15:01 +0530

    implement suggested users fetching

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,34 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { baseUrl } from "../../constant/url.js";
 
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`${baseUrl}/api/users/suggested`, {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Failed to fetch suggested users");
+        }
+        return data;
+      } catch (error) {
+        throw error;
+      }
+    },
+  });
+
+  if (suggestedUsers === 0) {
+    return <div className="md:w-64 w-0"></div>;
+  }
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -20,7 +45,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
