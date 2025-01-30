5e8f8715ab560241a0f835c9e591fd03cdfe5eb6
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-30 14:02:19 +0530

    minor path fix

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.json(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.use("*", (req,res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   })
