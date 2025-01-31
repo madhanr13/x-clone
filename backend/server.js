c3556e6c78ef096b831db09b1c0b8be72d55af46
Madhan R <135804250+madhanr13@users.noreply.github.com>
2025-01-31 12:18:48 +0530

    fix: correct static file path for production environment

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.use("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   })
 }
 app.listen(PORT, () => {
